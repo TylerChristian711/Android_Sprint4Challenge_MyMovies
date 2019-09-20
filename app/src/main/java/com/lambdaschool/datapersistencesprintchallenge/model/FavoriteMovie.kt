fbb7d470b7e5a2b0471144e40cf4301f9e5d552f
TylerChristian711 <tylerc71197@gmail.com>
2019-09-20 14:08:05 -0400

    add Favoirtemovie class add dependicies

---
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/model/FavoriteMovie.kt
@@ -0,0 +1,6 @@
+package com.lambdaschool.datapersistencesprintchallenge.model
+
+class FavoriteMovie(val title:String,
+                    val release_date:String,
+                    var isFavorite:Boolean)
+
